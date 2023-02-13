511d7e879e3929a329f8330ac84902ede9509927
No4b3rt <norbert.darzecki@gmail.com>
2023-02-13 20:42:23 +0100

    Initial commit

---
+++ src/Library.java
@@ -0,0 +1,22 @@
+public class Library {
+        public static void main(String[] args) {
+            final String appName = "Biblioteka v0.1";
+
+            String title = "Java w 21 dni";
+            String author = "Rogers Cadenhead";
+            int realeseDate = 2016;
+            int pages = 600;
+            String publisher = "Helion";
+            String isbn = " 9788328326217";
+
+            System.out.println(appName);
+            System.out.println("Książki dostępne w bibliotece");
+            System.out.println(title);
+            System.out.println(author);
+            System.out.println(realeseDate);
+            System.out.println(pages);
+            System.out.println(publisher);
+            System.out.println(isbn);
+        }
+    }
+

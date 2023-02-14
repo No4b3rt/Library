73888110077ea9140d26dfec0b2333cacca76c25
No4b3rt <norbert.darzecki@gmail.com>
2023-02-14 18:42:06 +0100

    new book class

--- src/Library.java
+++ src/Library.java
@@ -1,22 +1,23 @@
 public class Library {
         public static void main(String[] args) {
-            final String appName = "Biblioteka v0.1";
+            final String appName = "Biblioteka v0.2";
 
-            String title = "Java w 21 dni";
-            String author = "Rogers Cadenhead";
-            int realeseDate = 2016;
-            int pages = 600;
-            String publisher = "Helion";
-            String isbn = " 9788328326217";
+            Book book1 = new Book();
+            book1.title = "Java w 21 dni";
+            book1.author = "Rogers Cadenhead";
+            book1.realeseDate = 2016;
+            book1.pages = 600;
+            book1.publisher = "Helion";
+            book1.isbn = "9788328326217";
 
             System.out.println(appName);
             System.out.println("Książki dostępne w bibliotece");
-            System.out.println(title);
-            System.out.println(author);
-            System.out.println(realeseDate);
-            System.out.println(pages);
-            System.out.println(publisher);
-            System.out.println(isbn);
+            System.out.println(book1.title);
+            System.out.println(book1.author);
+            System.out.println(book1.realeseDate);
+            System.out.println(book1.pages);
+            System.out.println(book1.publisher);
+            System.out.println(book1.isbn);
         }
     }
 
